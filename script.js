d677837b0053b9ed717dde4d18c57fe5d7d08f79
minhkonzz <minhphm37@gmail.com>
2022-11-25 19:53:46 +0700

    build sidebar for managing points - 1

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-// declare Point structure
-function Point(_x, _y) {
-    this.x = _x; 
-    this.y = _y; 
-}
+/* Get DOMElements */
+const addPointBtn = document.getElementById("add-point-btn"); 
+const pointsContainer = document.querySelector(".points-container"); 
+const XInput = document.getElementById("x-inp"); 
+const YInput = document.getElementById("y-inp"); 
+
+/* Initialize custom variables */
+let points = []; 
+
+
+addPointBtn.addEventListener("click", function() {
+  const XValue = XInput.value; 
+  const YValue = YInput.value; 
+  console.log(XValue, YValue); 
+});
